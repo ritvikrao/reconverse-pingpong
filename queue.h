c6a0166135dac1ba73ea3cdc54a7d4052d53a3f6
Maya Taylor <mayat4@illinois.edu>
2025-01-29 14:22:18 -0600

    rename queue

--- queue.h
+++ queue.h
@@ -3,30 +3,33 @@
 
 #include <queue>
 #include <mutex>
-class MutexAccessControl {
+class MutexAccessControl
+{
 public:
     static std::mutex mutex;
-    
-    static void acquire() {
+
+    static void acquire()
+    {
         mutex.lock();
     }
-    
-    static void release() {
+
+    static void release()
+    {
         mutex.unlock();
     }
 };
 
 std::mutex MutexAccessControl::mutex;
 
-
-
 // An MPSC queue that can be used to send messages between threads.
-template<typename ConcreteQ, typename MessageType, typename AccessControlPolicy>
-class MPSCQueue {
+template <typename ConcreteQ, typename MessageType, typename AccessControlPolicy>
+class MPSCQueue
+{
     ConcreteQ q;
 
 public:
-    MessageType pop() {
+    MessageType pop()
+    {
         AccessControlPolicy::acquire();
         // This will not work for atomics.
         // It's fine for now: internal implementation detail.
@@ -36,15 +39,15 @@ public:
         return message;
     }
 
-    void push(MessageType message) {
+    void push(MessageType message)
+    {
         AccessControlPolicy::acquire();
         q.push(message);
         AccessControlPolicy::release();
     }
-
 };
 
-template<typename MessageType>
-using ReconverseQueue = MPSCQueue<std::queue<MessageType>, MessageType, MutexAccessControl>;
+template <typename MessageType>
+using ConverseQueue = MPSCQueue<std::queue<MessageType>, MessageType, MutexAccessControl>;
 
 #endif
--- test/queue.C
+++ test/queue.C
@@ -6,29 +6,33 @@
 #include <mutex>
 #include <assert.h>
 
-bool testSimplePushPop() {
-    ReconverseQueue<int> q;
+bool testSimplePushPop()
+{
+    ConverseQueue<int> q;
     q.push(1);
     q.push(2);
     q.push(3);
     return q.pop() == 1 && q.pop() == 2 && q.pop() == 3;
 }
 
-bool testMultiThreadedPushPop() {
-    ReconverseQueue<int> q;
+bool testMultiThreadedPushPop()
+{
+    ConverseQueue<int> q;
     const int NUM_THREADS = 10;
     const int ITEMS_PER_THREAD = 2000000;
-    
+
     // Barrier implementation using condition variable
     // std::barrier is not available in C++11
     std::mutex mutex;
     std::condition_variable cv;
     int count = 0;
     bool ready = false;
-    
+
     std::vector<std::thread> threads;
-    for (int t = 0; t < NUM_THREADS; t++) {
-        threads.push_back(std::thread([&, t]() {
+    for (int t = 0; t < NUM_THREADS; t++)
+    {
+        threads.push_back(std::thread([&, t]()
+                                      {
             {
                 std::unique_lock<std::mutex> lock(mutex);
                 count++;
@@ -43,28 +47,32 @@ bool testMultiThreadedPushPop() {
             // Push values
             for (int i = 0; i < ITEMS_PER_THREAD; i++) {
                 q.push(t * ITEMS_PER_THREAD + i);
-            }
-        }));
+            } }));
     }
-    
-    for (auto& thread : threads) {
+
+    for (auto &thread : threads)
+    {
         thread.join();
     }
-    
-    std::vector<int> values;    
-    for (int i = 0; i < NUM_THREADS * ITEMS_PER_THREAD; i++) {
+
+    std::vector<int> values;
+    for (int i = 0; i < NUM_THREADS * ITEMS_PER_THREAD; i++)
+    {
         values.push_back(q.pop());
     }
     std::sort(values.begin(), values.end());
-    for (int i = 0; i < NUM_THREADS * ITEMS_PER_THREAD; i++) {
-        if (values[i] != i) {
+    for (int i = 0; i < NUM_THREADS * ITEMS_PER_THREAD; i++)
+    {
+        if (values[i] != i)
+        {
             return false;
         }
     }
     return true;
 }
 
-int main() {
+int main()
+{
     assert(testSimplePushPop());
     assert(testMultiThreadedPushPop());
     return 0;
