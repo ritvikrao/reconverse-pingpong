8d93ad369e2e6987d483c818caa3c1b88c879d49
Maya Taylor <mayat4@illinois.edu>
2025-01-30 22:12:22 -0600

    not sure where cmi handler things should go

--- CpvMacros.h
+++ CpvMacros.h
@@ -9,38 +9,39 @@ CpvStaticDeclare(int, CmiHandlerCount);
 CpvStaticDeclare(int, CmiHandlerLocal);
 CpvStaticDeclare(int, CmiHandlerGlobal);
 CpvDeclare(int, CmiHandlerMax);
-static void CmiExtendHandlerTable(int atLeastLen)
-{
-    int max = CpvAccess(CmiHandlerMax);
-    int newmax = (atLeastLen + (atLeastLen >> 2) + 32);
-    int bytes = max * sizeof(CmiHandlerInfo);
-    int newbytes = newmax * sizeof(CmiHandlerInfo);
-    CmiHandlerInfo *nu = (CmiHandlerInfo *)malloc(newbytes);
-    CmiHandlerInfo *tab = CpvAccess(CmiHandlerTable);
-    _MEMCHECK(nu);
-    if (tab)
-    {
-        memcpy(nu, tab, bytes);
-    }
-    memset(((char *)nu) + bytes, 0, (newbytes - bytes));
-    free(tab);
-    tab = nu;
-    CpvAccess(CmiHandlerTable) = tab;
-    CpvAccess(CmiHandlerMax) = newmax;
-}
-void CmiNumberHandler(int n, CmiHandler h)
-{
-    CmiHandlerInfo *tab;
-    if (n >= CpvAccess(CmiHandlerMax))
-        CmiExtendHandlerTable(n);
-    tab = CpvAccess(CmiHandlerTable);
-    tab[n].hdlr = (CmiHandlerEx)h; /* LIE!  This assumes extra pointer will be ignored!*/
-    tab[n].userPtr = 0;
-}
-int CmiRegisterHandler(CmiHandler h)
-{
-    int Count = CpvAccess(CmiHandlerCount);
-    CmiNumberHandler(Count, h);
-    CpvAccess(CmiHandlerCount) = Count + DIST_BETWEEN_HANDLERS;
-    return Count;
-}
+
+// static void CmiExtendHandlerTable(int atLeastLen)
+// {
+//     int max = CpvAccess(CmiHandlerMax);
+//     int newmax = (atLeastLen + (atLeastLen >> 2) + 32);
+//     int bytes = max * sizeof(CmiHandlerInfo);
+//     int newbytes = newmax * sizeof(CmiHandlerInfo);
+//     CmiHandlerInfo *nu = (CmiHandlerInfo *)malloc(newbytes);
+//     CmiHandlerInfo *tab = CpvAccess(CmiHandlerTable);
+//     _MEMCHECK(nu);
+//     if (tab)
+//     {
+//         memcpy(nu, tab, bytes);
+//     }
+//     memset(((char *)nu) + bytes, 0, (newbytes - bytes));
+//     free(tab);
+//     tab = nu;
+//     CpvAccess(CmiHandlerTable) = tab;
+//     CpvAccess(CmiHandlerMax) = newmax;
+// }
+// void CmiNumberHandler(int n, CmiHandler h)
+// {
+//     CmiHandlerInfo *tab;
+//     if (n >= CpvAccess(CmiHandlerMax))
+//         CmiExtendHandlerTable(n);
+//     tab = CpvAccess(CmiHandlerTable);
+//     tab[n].hdlr = (CmiHandlerEx)h; /* LIE!  This assumes extra pointer will be ignored!*/
+//     tab[n].userPtr = 0;
+// }
+// int CmiRegisterHandler(CmiHandler h)
+// {
+//     int Count = CpvAccess(CmiHandlerCount);
+//     CmiNumberHandler(Count, h);
+//     CpvAccess(CmiHandlerCount) = Count + DIST_BETWEEN_HANDLERS;
+//     return Count;
+// }
